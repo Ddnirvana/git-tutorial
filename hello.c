33268b1fc0eb84b752dfc84907dda6b02ab7f485
Dong Du <Dd_nirvana@sjtu.edu.cn>
2021-09-15 10:18:16 +0800

    add debug log
    Signed-off-by: Dong Du <Dd_nirvana@sjtu.edu.cn>

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+void debug_func(void){
+	static int debug_flag = 0;
+	fprintf(stderr, "Debug flag-%d triggered\n", debug_flag++);
+}
 int main(){
+	debug_func();
 	printf("hello world\n");
+	debug_func();
 	return 0;
+	debug_func();
 }
